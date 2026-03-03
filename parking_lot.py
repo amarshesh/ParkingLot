83fa1ebbd2d16eecce5bcdc7579123cd1fd6cb7f
Amarshesh Sharma <amarsheshsit@rediffmail.com>
2026-03-03 12:25:51 +0530

    Changed logic for object creation using factory method

--- parking_lot.py
+++ parking_lot.py
@@ -1,8 +1,8 @@
 import uuid
 import time
+from pricingStrategy.pricing_factory import VehiclePricingFactory
 from spot_class import SpotClass
 from ticket_class import Ticket
-from pricingStrategy.vehicles_pricing import SmallPricing, MediumPricing, LargePricing
 class ParkingLot:
     def __init__(self):
         self.spots = []
@@ -17,12 +17,7 @@ class ParkingLot:
                 spot.current_vehicle = vehicle_number
                 ticket_id = str(uuid.uuid4())
                 entry_time = time.time()
-                if vehicle_size == "small":
-                    pricing_strategy = SmallPricing()
-                elif vehicle_size == "medium":
-                    pricing_strategy = MediumPricing()
-                elif vehicle_size == "large":
-                    pricing_strategy = LargePricing()
+                pricing_strategy = VehiclePricingFactory.get_pricing_strategy(vehicle_size)
                 ticket = Ticket(ticket_id, entry_time, spot.spot_id, vehicle_size, pricing_strategy)
                 self.tickets[ticket_id] = ticket    
                 return ticket_id
--- pricingStrategy/pricing_strategy.py
+++ pricingStrategy/pricing_strategy.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 # this is an starategy pattern, we can have different pricing strategies like discount, premium, etc.
 class PricingStrategy( ABC):
     @abstractmethod
